532d1238c1060fa8539bef7379fa5353b3a53df0
nam lai <lai.nam0210@gmail.com>
2018-08-13 20:06:38 +0900

    update run with unicode

--- test_vocab.py
+++ test_vocab.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 import gensim
 
 
 def log(word):
-    print('{0} with prob: {1:2f}'.format(word[0], word[1]))
+    print('{0} with prob: {1:2f}'.format(word[0].encode('utf-8'), word[1]))
 
 
 # Loading word2Vec model
 w2v_model = gensim.models.Word2Vec.load('./model_18394.model')
 
 # try top 5 similar_by_word
-tuples = w2v_model.wv.similar_by_word('điện_thoại', topn=5)
+tuples = w2v_model.wv.similar_by_word(u'điện_thoại', topn=5)
 
 """
 output
@@ -26,18 +27,18 @@ for word in tuples:
 # output: con_gái
 print("====================================================================")
 print("doesnt_match of: [điện_thoại iphone ipad con_gái]")
-print(w2v_model.wv.doesnt_match("điện_thoại iphone ipad con_gái".split()))
+print(w2v_model.wv.doesnt_match(u"điện_thoại iphone ipad con_gái".split()))
 
 # output: sách
-print("doesnt_match of: [sách lập_trình máy_tính phần_mềm]")
-print(w2v_model.wv.doesnt_match("sách lập_trình máy_tính phần_mềm".split()))
+print(u"doesnt_match of: [sách lập_trình máy_tính phần_mềm]")
+print(w2v_model.wv.doesnt_match(u"sách lập_trình máy_tính phần_mềm".split()))
 
 # output: mềm
-print("doesnt_match of: [tím xanh đỏ mềm]")
-print(w2v_model.wv.doesnt_match("tím xanh đỏ mềm".split()))
+print(u"doesnt_match of: [tím xanh đỏ mềm]")
+print(w2v_model.wv.doesnt_match(u"tím xanh đỏ mềm".split()))
 
 print("====================================================================")
 
 
 # access word2vector
-print(w2v_model.wv['điện_thoại'])
+print(w2v_model.wv[u'điện_thoại'])
